b3958041ef44e45f88ff0955da7a3e721798d7ad
Mesoon-copilot <moojiblackstar@gmail.com>
2026-03-23 16:13:35 +0700

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,6 +1,7 @@
 window.APP_MENU = [
   { key: "dashboard", label: "Dashboard", href: "dashboard.html", icon: "🏠" },
   { key: "print", label: "Print Label", href: "label-print-app.html", icon: "🏷️" },
+   { key: "print10", label: "Print Label 10x7.5", href: "label-print-app10x7.5 .html", icon: "🏷️" },
   { key: "manage", label: "Manage", href: "manage-app.html", icon: "🗂️" },
   { key: "report", label: "Report", href: "report-app.html", icon: "📊" }
 ];
